192d57aa040865ad6cc1d6ec3ec8a4c49e93f91a
icoldplayer <slashsdull@gmail.com>
2019-05-18 03:19:49 +0700

    customizing admin dashboard

--- blog/admin.py
+++ blog/admin.py
@@ -9,10 +9,18 @@ admin.site.site_title = "iColdPlayer Administrator"
 admin.site.site_index = "iColdPlayer"
 
 
+
 class Blog(admin.ModelAdmin):
     formfield_overrides = {
         models.TextField: {'widget': AdminMartorWidget},
     }
 
+class PostAdmin(admin.ModelAdmin):
+    list_display = ( 'title', 'created', 'updated', 'author')
+    list_filter = ('created', 'updated')
+    search_fields = ('title', 'slug')
+    list_display_links = ('created', 'updated')
+    list_editable = ('title','author')
 
-admin.site.register(Post)
+
+admin.site.register(Post, PostAdmin)
